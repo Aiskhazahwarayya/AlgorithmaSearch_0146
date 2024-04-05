f5c64fa535778f5ec0ca74ac835742a5ddb059bb
Aiskhazahwarayya <aiskharayya307@gmail.com>
2024-04-05 11:21:04 +0700

    mengeluarkan if jika kondisi terpenuhi

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -52,7 +52,12 @@ void LinearSearch()
             }
             i++;
         }
+        if (i == n)
+            cout << "\n" << item << " not found in the array\n";
+        cout << "\nNumber of comparisons: " << ctr << endl;
 
+        cout << "\nContinue search (y/n): ";
+        cin >> ch;
     } 
 }
 
